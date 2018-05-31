0cb1e2a7049e3e4b1a57e410c1aff2b5e072183d
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-05-31 17:54:27 -0300

    funfando o socket

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const server = http.Server(app);
 const io = socketIO(server);
 const store = new expressSession.MemoryStore();
 
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressSession({
